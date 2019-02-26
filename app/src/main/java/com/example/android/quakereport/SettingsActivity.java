bea3c9c9ffdf95856c7e2b04eff29b09a7cf8cde
BMontijo <juansonos@gmail.com>
2019-02-26 15:07:04 -0500

    Add current setting to preference fragment

--- app/src/main/java/com/example/android/quakereport/SettingsActivity.java
+++ app/src/main/java/com/example/android/quakereport/SettingsActivity.java
@@ -1,4 +1,5 @@
 package com.example.android.quakereport;
+import android.content.SharedPreferences;
 import android.support.v7.app.*;
 import android.os.*;
 import android.preference.*;
@@ -13,11 +14,28 @@ public class SettingsActivity extends AppCompatActivity
 		setContentView(R.layout.settings_activity);
 	}
 	
-	public static class EarthquakePreferenceFragment extends PreferenceFragment {
+	public static class EarthquakePreferenceFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
 		@Override
 		public void onCreate(Bundle savedInstanceState) {
 			super.onCreate(savedInstanceState);
 			addPreferencesFromResource(R.xml.settings_main);
+
+			Preference minMagnitude = findPreference(getString(R.string.min_mag_key_settings));
+			bindPreferenceToValue(minMagnitude);
+		}
+
+		private void bindPreferenceToValue(Preference minMagnitude) {
+			minMagnitude.setOnPreferenceChangeListener(this);
+			SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(minMagnitude.getContext());
+			String preferenceString = preferences.getString(minMagnitude.getKey(), "");
+			onPreferenceChange(minMagnitude, preferenceString);
+		}
+
+		@Override
+		public boolean onPreferenceChange(Preference preference, Object value) {
+			String stringValue = value.toString();
+			preference.setSummary(stringValue);
+			return true;
 		}
 	}
 }
